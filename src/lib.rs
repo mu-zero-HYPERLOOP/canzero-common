31c1aae7693da9f9bf834b001731922df5dc25ad
kistenklaus <karlsasssie@gmail.com>
2024-04-25 21:42:27 +0200

    use serde derive Serialize, Deserialize for consistancy
    will make dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,29 @@
-
+use serde::{
+    Deserialize, Serialize,
+};
 use std::{ops::Deref, time::Duration};
-use serde::{ser::SerializeMap, Serialize};
 
 pub struct Timestamped<T> {
-    timestamp : Duration,
-    value : T,
+    timestamp: Duration,
+    value: T,
 }
+
+// TODO serialize implementation for Timestamped<T : Serialize>
+// impl<T> Serialize for Timestamped<T>
+// where
+//     T: Serialize,
+// {
+//     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+//     where
+//         S: serde::Serializer,
+//     {
+//         let mut stru = serializer.serialize_struct("Timestamped", 2)?;
+//         stru.serialize_field("timestamp", &self.timestamp)?;
+//         stru.serialize_field("value", &self.value)?;
+//         stru.end()
+//     }
+// }
+//TODO deserialize implementation for Timestamped<T : Deserialize>
 
 impl<T> Deref for Timestamped<T> {
     type Target = T;
@@ -16,11 +34,8 @@ impl<T> Deref for Timestamped<T> {
 }
 
 impl<T> Timestamped<T> {
-    pub fn new(timestamp : Duration, value : T) -> Self{
-        Self {
-            timestamp,
-            value
-        }
+    pub fn new(timestamp: Duration, value: T) -> Self {
+        Self { timestamp, value }
     }
 
     pub fn timestamp(&self) -> &Duration {
@@ -31,15 +46,14 @@ impl<T> Timestamped<T> {
         (self.timestamp, self.value)
     }
 
-    pub fn new_value<R>(&self, value : R) -> Timestamped<R> {
+    pub fn new_value<R>(&self, value: R) -> Timestamped<R> {
         Timestamped::new(*self.timestamp(), value)
     }
 }
 
-
 pub type TCanFrame = Timestamped<CanFrame>;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct CanFrame {
     id: u32,
     dlc: u8,
@@ -106,25 +120,27 @@ impl CanFrame {
     }
 }
 
-impl Serialize for CanFrame {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer {
-        let mut map = serializer.serialize_map(Some(5))?;
-        let id = self.id & CanFrameIdFlags::ExtMask as u32;
-        map.serialize_entry("id", &id)?;
-        let ide = (self.id & CanFrameIdFlags::IdeMask as u32) != 0;
-        map.serialize_entry("ide", &ide)?;
-        let rtr = (self.id & CanFrameIdFlags::RtrMask as u32) != 0;
-        map.serialize_entry("rtr", &rtr)?;
-        map.serialize_entry("dlc", &self.dlc)?;
-        map.serialize_entry("data", &self.data)?;
-        map.end()
-    }
-}
+// impl Serialize for CanFrame {
+//     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+//     where
+//         S: serde::Serializer,
+//     {
+//         let mut map = serializer.serialize_map(Some(5))?;
+//         let id = self.id & CanFrameIdFlags::ExtMask as u32;
+//         map.serialize_entry("id", &id)?;
+//         let ide = (self.id & CanFrameIdFlags::IdeMask as u32) != 0;
+//         map.serialize_entry("ide", &ide)?;
+//         let rtr = (self.id & CanFrameIdFlags::RtrMask as u32) != 0;
+//         map.serialize_entry("rtr", &rtr)?;
+//         map.serialize_entry("dlc", &self.dlc)?;
+//         map.serialize_entry("data", &self.data)?;
+//         map.end()
+//     }
+// }
 
 pub type TCanError = Timestamped<CanError>;
 
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct CanError(pub u64);
 
 impl CanError {
@@ -133,37 +149,37 @@ impl CanError {
     }
 }
 
-impl Serialize for CanError {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        let mut map = serializer.serialize_map(Some(3))?;
-
-        let bits = self.0;
-        map.serialize_entry("data", &bits)?;
-        if bits & 1 != 0 {
-            map.serialize_entry("name", "CAN Bit Error")?;
-            map.serialize_entry("description", "Wtf i didn't send that shit")?;
-        } else if bits & 2 != 0 {
-            map.serialize_entry("name", "CAN Bit Stuffing Error")?;
-            map.serialize_entry("description", "Whhyy is everybody sending bullshit!")?;
-        } else if bits & 4 != 0 {
-            map.serialize_entry("name", "CAN Form Error")?;
-            map.serialize_entry(
-                "description",
-                "Somebody in this network is to stupid to follow CAN standards!",
-            )?;
-        } else if bits & 8 != 0 {
-            map.serialize_entry("name", "CAN ACK Error")?;
-            map.serialize_entry("description", "Wait what CAN has ACK!")?;
-        } else if bits & 16 != 0 {
-            map.serialize_entry("name", "CAN CRC Error")?;
-            map.serialize_entry("description", "Some CRC was computed incorrectly!")?;
-        } else {
-            map.serialize_entry("name", "Internal Error")?;
-            map.serialize_entry("description", "The CNL fucked up some how!")?;
-        }
-        map.end()
-    }
-}
+// impl Serialize for CanError {
+//     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+//     where
+//         S: serde::Serializer,
+//     {
+//         let mut map = serializer.serialize_map(Some(3))?;
+//
+//         let bits = self.0;
+//         map.serialize_entry("data", &bits)?;
+//         if bits & 1 != 0 {
+//             map.serialize_entry("name", "CAN Bit Error")?;
+//             map.serialize_entry("description", "Wtf i didn't send that shit")?;
+//         } else if bits & 2 != 0 {
+//             map.serialize_entry("name", "CAN Bit Stuffing Error")?;
+//             map.serialize_entry("description", "Whhyy is everybody sending bullshit!")?;
+//         } else if bits & 4 != 0 {
+//             map.serialize_entry("name", "CAN Form Error")?;
+//             map.serialize_entry(
+//                 "description",
+//                 "Somebody in this network is to stupid to follow CAN standards!",
+//             )?;
+//         } else if bits & 8 != 0 {
+//             map.serialize_entry("name", "CAN ACK Error")?;
+//             map.serialize_entry("description", "Wait what CAN has ACK!")?;
+//         } else if bits & 16 != 0 {
+//             map.serialize_entry("name", "CAN CRC Error")?;
+//             map.serialize_entry("description", "Some CRC was computed incorrectly!")?;
+//         } else {
+//             map.serialize_entry("name", "Internal Error")?;
+//             map.serialize_entry("description", "The CNL fucked up some how!")?;
+//         }
+//         map.end()
+//     }
+// }
